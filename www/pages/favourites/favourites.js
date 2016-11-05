954588401c92897d37ea1989ae5c6b389aded90b
Leonel Viera <vieraleonel1@gmail.com>
2016-11-05 15:24:14 -0300

    Favorites page

--- www/pages/favourites/favourites.js
+++ www/pages/favourites/favourites.js
@@ -1,53 +1,32 @@
-(function($$, T7, app, ns) {
+(function($$, app, fs) {
     'use strict';
 
-    /**
-     * Returns next page. Checks if current page is last
-     * @return string page (nestoria pages are strings)
-     */
-    function getNextPage() {
-        return (app.template7Data['page:results'].page < app.template7Data['page:results'].pages ? app.template7Data['page:results'].page + 1 : app.template7Data['page:results'].pages);
+    function goToPropertyDetails(property) {
+        property.isFav = true;
+        app.template7Data['page:property-details'] = property;
+
+        mainView.loadPage('pages/property-details/property-datails.html');
     }
+    
+    app.onPageBeforeAnimation('favourites', function(page){
+        fs.getAll(favouritesSuccess);
 
-    function loadMore() {
-        if (app.template7Data['page:results'].page < app.template7Data['page:results'].pages) {
-            ns.searchProperties(
-                app.template7Data['page:results'].searchTerm, 
-                getNextPage(), 
-                function(data, status, xhr) {
-
-                    if (data.response.application_response_code == 100 || 
-                        data.response.application_response_code == 101 ||
-                        data.response.application_response_code == 110) {
-
-                        app.template7Data['page:results'].page       = data.response.page;
-                        app.template7Data['page:results'].more       = data.response.page < data.response.total_pages;
-                        app.template7Data['page:results'].properties = app.template7Data['page:results'].properties.concat(data.response.listings);
-                        app.template7Data['page:results'].showing    = app.template7Data['page:results'].properties.length;
-
-                        mainView.router.refreshPage();
-                    } else {
-                        alert(error);
-                    }
-                }
-            );
+        function favouritesSuccess(faves) {
+            app.template7Data['page:favourites'] = {favourites: faves};
+            var html = window.favouritesListCompiled( app.template7Data['page:favourites']);
+            $$('.favourites-list-container').html(html); 
         }
-    }
-
-    app.onPageInit('results', function(page) {
-
-        $$('#results-list').on('click', '.property-item', function(event) {
-            var index = $$(this).data('index');
-            app.template7Data['page:property-details'] = app.template7Data['page:results'].properties[index];
-            mainView.loadPage('pages/property-datails.html');
-        });
-
-        $$('#load-more-results').on('click', function(event){
-            event.preventDefault();
-
-            $$('#load-more-results .item-title').html('Loading...');
-
-            loadMore();
-        }); 
     });
-})(Dom7, Template7, app, nestoriaService);
+
+    app.onPageInit('favourites', function(page) {
+
+        // go to property details event
+        $$('.favourites-list-container').on('click', '.property-item', function(event) {
+            event.preventDefault();
+            
+            var index = $$(this).data('index');
+
+            goToPropertyDetails(app.template7Data['page:favourites'].favourites[index]);
+        });
+    });
+})(Dom7, app, favouritesService);
